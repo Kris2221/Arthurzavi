3513e231c6e67b4910b8767b3a963874095a42e4
ZAVI Treinador <noreply@users.noreply.github.com>
2026-04-22 12:12:21 -0300

    ci: deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: process.env.GITHUB_ACTIONS ? `/${process.env.GITHUB_REPOSITORY?.split("/")[1] ?? ""}/` : "/",
   server: {
     host: "::",
     port: 8080,
